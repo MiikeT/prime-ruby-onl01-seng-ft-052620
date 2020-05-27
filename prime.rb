5bd9a7b24d6582c2efa511fe0ecc188a29fdda6a
Michael Takeuchi <miketaki@sbcglobal.net>
2020-05-27 00:42:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,12 @@
 def prime?(int)
   return false if int < 2
   
-  (2..int).each do |num|
-    if int % num == 0
+  i = 2
+  while i < num
+    if num % i == 0
       return false
     end
+    i += 1
   end
   true
 end
